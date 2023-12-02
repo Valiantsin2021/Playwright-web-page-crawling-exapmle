a014b6433a2aeab48f9f41879b7f2f77b42b93c5
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2023-12-02 09:08:50 +0100

    refactor image_scraper.spec.js to add batch request processing

--- tests/image_scraper.spec.js
+++ tests/image_scraper.spec.js
@@ -2,10 +2,11 @@ import { test, expect } from '@playwright/test'
 import fs from 'fs'
 /* eslint-disable playwright/no-conditional-in-test */
 /* eslint-disable playwright/expect-expect */
-test(`Parse and save the images from `, async ({ request, page }) => {
+test.only(`Parse and save the images using request, recursion and Promise.all() `, async ({ request, page }) => {
   test.setTimeout(3_600_000)
   await page.goto('https://altphotos.com/gallery/')
-  fs.existsSync('./downloaded_images') ? console.log('folder ready') : fs.mkdirSync('./downloaded_images')
+  const folderPath = './downloaded_images'
+  fs.existsSync(folderPath) ? console.log('Folder already exists') : fs.mkdirSync(folderPath)
   const nextBtn = page.getByText('Show me more...')
   const images = await page
     .locator('a')
@@ -18,13 +19,28 @@ test(`Parse and save the images from `, async ({ request, page }) => {
   }
   let length = sources.length
   async function recurse(sources) {
-    for (let el of sources) {
-      const imageName = `image_${Date.now()}.jpg`
-      const response = await request.get(el)
-      fs.writeFile(`./downloaded_images/${imageName}`, await response.body(), 'binary', error => {})
-    }
+    const downloadPromises = sources.map(async source => {
+      try {
+        const imageName = `image_${Date.now()}.jpg`
+        const response = await request.get(source)
+        fs.writeFile(`${folderPath}/${imageName}`, await response.body(), 'binary', error => {
+          error ? console.error(error) : console.log(`Image ${imageName} downloaded successfully`)
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    })
+    await Promise.all(downloadPromises)
+    // for (let el of sources) {
+    //   const imageName = `image_${Date.now()}.jpg`
+    //   const response = await request.get(el)
+    //   fs.writeFile(`${folderPath}/${imageName}`, await response.body(), 'binary', error => {
+    //     error ? console.error(error) : console.log(`Image ${imageName} downloaded successfully`)
+    //   })
+    // }
     const nextBtnlength = await nextBtn.count()
     if (nextBtnlength > 0) {
+      console.log(`Loading new page`)
       await nextBtn.click({ force: true })
       await page.waitForTimeout(5000)
       const images = await page
@@ -45,4 +61,61 @@ test(`Parse and save the images from `, async ({ request, page }) => {
     }
   }
   await recurse(sources)
+})
+test(`Variant with while loop`, async ({ page, request }) => {
+  test.setTimeout(3_600_000)
+  const folderPath = './downloaded_images'
+  fs.existsSync(folderPath) ? console.log('folder ready') : fs.mkdirSync(folderPath)
+  await page.goto('https://altphotos.com/gallery/')
+  const nextBtn = page.getByText('Show me more...')
+
+  async function downloadImage(source) {
+    const imageName = `image_${Date.now()}.jpg`
+    const response = await request.get(source)
+    await fs.writeFile(`${folderPath}/${imageName}`, await response.body(), 'binary', err => err)
+    console.log(`Image ${imageName} downloaded successfully`)
+  }
+
+  async function parseImages() {
+    const images = await page
+      .locator('a')
+      .filter({ has: page.locator('img') })
+      .all()
+    const sources = []
+
+    for (const image of images) {
+      const source = await image.getAttribute('href')
+      sources.push(`https://altphotos.com/download/${source?.match(/\d{4}/)[0]}`)
+    }
+
+    return sources
+  }
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const sources = await parseImages()
+
+    for (const source of sources) {
+      try {
+        await downloadImage(source)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    const nextBtnlength = await nextBtn.count()
+    if (nextBtnlength > 0) {
+      console.log('Loading new page')
+      await nextBtn.click({ force: true })
+      await page.waitForTimeout(5000)
+
+      const newSources = await parseImages()
+      expect(newSources.length).toBeGreaterThan(sources.length)
+
+      sources.push(...newSources)
+    } else {
+      console.log('No more images')
+      break
+    }
+  }
 })
